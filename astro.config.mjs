3aef7bad393bc64acab5dd9307eb0fcc1d651a89
Jose Espejo <joseespejo@yahoo.com>
2024-09-05 11:40:42 +0100

    chore: Update Astro config to include solid and react integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import solid from '@astrojs/solid-js';
+import react from '@astrojs/react';
 
-import react@2.1.1 from "@astrojs/react";
-import solidJs@2.1.0 from "@astrojs/solid-js";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react@2.1.1(), solidJs@2.1.0()]
+	output: 'server',
+	site: 'https://astro-frontend-masters.netlify.app',
+	experimental: {
+		assets: true,
+	},
+	integrations: [react(), solid()],
 });
