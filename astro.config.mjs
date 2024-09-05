64e3c5cf9ee91cf5fb8edb1bdb9c6b231afc4d8d
Jose Espejo <joseespejo@yahoo.com>
2024-09-05 18:06:37 +0100

    chore: Update Astro config to include netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify/functions';
 
+// https://astro.build/config
 export default defineConfig({
-	output: 'server',
 	site: 'https://astro-frontend-masters.netlify.app',
+	output: 'hybrid',
 	experimental: {
 		assets: true,
 	},
 	integrations: [react(), solid()],
+	adapter: netlify(),
 });
